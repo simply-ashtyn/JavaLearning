79a171eb1c3ccb5375a1ee20a23701425d968597
Ashtyn Lane <97042764+simply-ashtyn@users.noreply.github.com>
2025-06-06 23:24:42 -0500

    Ch 5: Functions

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 //Learning Java 11
 
 public class HelloWorld {
+
+    public static void announceDeveloperTeaTime()
+    {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type anything and press enter to start developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
+
+    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate)
+    {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        return result;
+    }
+
     public static void main(String[] args)
     {
         //BASIC DATA TYPES
@@ -27,10 +47,11 @@ public class HelloWorld {
         System.out.println(studentFullName + "has a new GPA of " + studentGPA);
 */
 
-        //CONTROL FLOW STATEMENTS
-        //If statements
 
-     /*  System.out.println("Pick a number between 1 and 10.");
+        //CH 3 - CONTROL FLOW STATEMENTS
+        //If statements
+/*
+        System.out.println("Pick a number between 1 and 10.");
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         if(number < 5)
@@ -59,6 +80,35 @@ public class HelloWorld {
         System.out.println("Playing next song");
 */
 
+        //NO CODE FOR CH 4 - GOES OVER DEBUGGING
+
+        //CH 5 - FUNCTOINS
+/*
+        announceDeveloperTeaTime();
+
+        double groupMealCost = calculateTotalMealPrice(90, 0.10, 0.08);
+        double individualMealCost = groupMealCost / 5;
+        System.out.println(individualMealCost);
+    
+        ///CODING CHALLENGE
+        static String moveCapitalLettersToFront(String s)
+        {
+                    StringBuilder result = new StringBuilder();
+        int capitalIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(Character.isUpperCase(ch))
+            {
+                result.insert(capitalIndex++,ch);
+            }
+            else
+            {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+        }
+*/
 
     }
 }
